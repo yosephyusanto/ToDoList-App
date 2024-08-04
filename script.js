cb9d884723ff5fbabb4c54e95e32f76c5742b21d
Yoseph Yusanto <yosephyusanto@gmail.com>
2024-08-04 17:45:54 +0700

    save todo to local storage

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const todoFilter = document.querySelector(".filter")
 btnSubmitTodo.addEventListener("click", addTodo)
 todoList.addEventListener("click", deleteTodo)
 todoFilter.addEventListener("click", filterTodo)
+document.addEventListener("DOMContentLoaded", loadTodosFromLocalStorage)
 
 
 //Functions
@@ -15,12 +16,16 @@ function addTodo(event){
   //Prevent browser from refresh when form is being submitted
   event.preventDefault()
   
+  //Create todo container for paragraph, check button, and trash button
   const todoItem = document.createElement("li")
   todoItem.classList.add("todo-item")
-
+  //Create paragraph 
   const todoContent = document.createElement("p")
   todoContent.classList.add("todo-content")
   todoContent.innerText = inputTodo.value
+  //Save todo to local storage
+  saveLocalTodo(inputTodo.value)
+  //Check and trash button
   const checkButton= document.createElement("button")
   checkButton.classList.add("check-btn")
   const trashButton = document.createElement("button")
@@ -35,7 +40,6 @@ function addTodo(event){
 
   //Remove text from input tag when form already submit
   inputTodo.value = ""
-    
 }
 
 function deleteTodo(event){
@@ -87,3 +91,80 @@ function filterTodo(event){
   })
 }
 
+function saveLocalTodo(todo){
+  //Check if the browser has localstorage or not
+  if(!isStorageExist())return
+
+  //Create array that will be store into localstorage
+  let todos;
+
+  const STORAGE_KEY = "todos"
+  //Create empty array when there is no localstorage yet
+  if(localStorage.getItem(STORAGE_KEY) === null){
+    todos = []
+  }
+  else{
+    //Copy array from localstorage to array todos
+    todos = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    console.log(localStorage.getItem(STORAGE_KEY))
+  }
+  
+  //Update todos array with todo that just being inserted from input tag
+  todos.push(todo)
+  //Create localstorage and save updated todos array inside local storage
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+}
+
+function loadTodosFromLocalStorage(){
+  //Check if browser has local storage
+  if(!isStorageExist())return
+
+  //Create array that will be store into localstorage
+  let todos;
+
+  const STORAGE_KEY = "todos"
+  //Create empty array when there is no localstorage yet
+  if(localStorage.getItem(STORAGE_KEY) === null){
+    todos = []
+  }
+  else{
+    //Copy array from localstorage to array todos
+    todos = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    console.log(localStorage.getItem(STORAGE_KEY))
+  }
+
+  //Render todolist for each item in todos array
+  todos.forEach(function(todo){
+    //Create todo container for paragraph, check button, and trash button
+    const todoItem = document.createElement("li")
+    todoItem.classList.add("todo-item")
+    //Create paragraph 
+    const todoContent = document.createElement("p")
+    todoContent.classList.add("todo-content")
+    todoContent.innerText = todo
+    //Check and trash button
+    const checkButton= document.createElement("button")
+    checkButton.classList.add("check-btn")
+    const trashButton = document.createElement("button")
+    trashButton.classList.add("trash-btn")
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+
+    todoItem.append(todoContent, checkButton, trashButton)
+
+    const todoList = document.querySelector(".todo-list")
+    todoList.appendChild(todoItem)
+  })
+}
+
+function deleteLocalTodo(){
+
+}
+
+function isStorageExist(){
+  if(typeof(Storage) !== undefined)return true
+  else {
+    alert("Your browser doesn't support web storage")
+    return false
+  }
+}
